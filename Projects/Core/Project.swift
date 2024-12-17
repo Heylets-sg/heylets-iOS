e8a9420a4af1815a14b417656bc45a56800817e0
HELLOHIDI <fbgmlwo123@naver.com>
2024-12-17 13:48:18 +0900

    [Feat] 모듈 생성 #5

---
+++ Projects/Core/Project.swift
@@ -0,0 +1,20 @@
+//
+//  Project.swift
+//  Heylets-iOSManifests
+//
+//  Created by 류희재 on 12/17/24.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: "Core",
+    targets: [.dynamicFramework], //, .demo
+    internalDependencies: [
+        .Modules.thirdPartyLibs
+    ]
+)
+
+
---
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -0,0 +1,57 @@
+//
+//  Project+Templates.swift
+//  Heylets-iOSManifests
+//
+//  Created by 류희재 on 12/17/24.
+//
+
+import ProjectDescription
+import ConfigPlugin
+import EnvPlugin
+
+
+public extension Project {
+    /// Sumarry: 모듈을 만드는 함수
+    ///
+    /// Discussion/Overview
+    ///
+    /// - Parameters:
+    ///    - name: 프로젝트 이름 (모듈 이름)
+    ///    - targets: 빌드할 타겟의 대상
+    ///    - packages:SPM의 package
+    ///    - internalDependencies: 내부 의존성
+    ///    - externalDependencies: 외부 의존성
+    ///    - interfaceDependencies: 인터페이스 의존성
+    ///    - sources: 소스 파일
+    ///    - hasResourcces: 리소스 파일 포함 여부
+    static func makeModule(
+        name: String,
+        targets: Set<FeatureTarget> = Set([.staticFramework, .unitTest, .demo]),
+        packages: [Package] = [],
+        internalDependencies: [TargetDependency] = [],
+        externalDependencies: [TargetDependency] = [],
+        interfaceDependencies: [TargetDependency] = [],
+        sources: SourceFilesList? = nil,
+        hasResources: Bool = false
+    ) -> Project {
+        let dependencies: [TargetDependency] = internalDependencies + externalDependencies + interfaceDependencies
+        
+        let projectTargets: [Target] = TargetHandler.makeProjectTargets(
+            name: name,
+            hasResources: hasResources,
+            with: dependencies,
+            targets: targets
+        )
+        let projcetScheme: [Scheme] = SchemeProvider.makeProjectScheme(targets: targets, name: name)
+        
+        return Project(
+            name: name,
+            organizationName: env.workspaceName,
+            packages: packages,
+            settings: .settings(configurations: XCConfig.configurations),
+            targets: projectTargets,
+            schemes: projcetScheme
+        )
+    }
+}
+
