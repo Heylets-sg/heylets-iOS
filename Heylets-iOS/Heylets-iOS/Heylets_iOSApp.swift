19ddb6e58f903e0ef5eb44f83c0e23df5695a1cd
HELLOHIDI <fbgmlwo123@naver.com>
2024-12-17 12:12:41 +0900

    [Setting] tuist 세팅 #3

---
+++ Heylets-iOS/Heylets-iOS/Heylets_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heylets_iOSApp.swift
+//  Heylets-iOS
+//
+//  Created by 류희재 on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Heylets_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
