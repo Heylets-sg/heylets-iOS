f26441cee070b4dad853a54e9b5c9d868b2ea15e
HELLOHIDI <fbgmlwo123@naver.com>
2024-12-20 11:45:11 +0900

    [Feat] 필요없는 코드 주석 처리 #9

--- Projects/Features/BaseFeatureDependency/Sources/Navigation/NavigationDestination.swift
+++ Projects/Features/BaseFeatureDependency/Sources/Navigation/NavigationDestination.swift
@@ -1,27 +1,27 @@
+////
+////  NavigationDestination.swift
+////  BaseFeatureDependency
+////
+////  Created by 류희재 on 12/19/24.
+////  Copyright © 2024 Heylets-iOS. All rights reserved.
+////
 //
-//  NavigationDestination.swift
-//  BaseFeatureDependency
+//import SwiftUI
 //
-//  Created by 류희재 on 12/19/24.
-//  Copyright © 2024 Heylets-iOS. All rights reserved.
+//public enum NavigationDestination: Hashable {
+//    case onboarding
+//    
+//    //signup
+//    case selectUniversity
+//    case verifyEmail
+//    case enterSecurityCode
+//    case enterPersonalInfo
+//    case enterIdPassword
+//    case addProfile
+//    
+//    //signin
+//    case login
+//    case enterEmail
+//    case resetPassword
+//}
 //
-
-import SwiftUI
-
-public enum NavigationDestination: Hashable {
-    case onboarding
-    
-    //signup
-    case selectUniversity
-    case verifyEmail
-    case enterSecurityCode
-    case enterPersonalInfo
-    case enterIdPassword
-    case addProfile
-    
-    //signin
-    case login
-    case enterEmail
-    case resetPassword
-}
-
--- Projects/Features/BaseFeatureDependency/Sources/Navigation/NavigationRoutable.swift
+++ Projects/Features/BaseFeatureDependency/Sources/Navigation/NavigationRoutable.swift
@@ -1,20 +1,20 @@
+////
+////  NavigationRoutable.swift
+////  BaseFeatureDependency
+////
+////  Created by 류희재 on 12/19/24.
+////  Copyright © 2024 Heylets-iOS. All rights reserved.
+////
 //
-//  NavigationRoutable.swift
-//  BaseFeatureDependency
+//import Foundation
+//import Combine
 //
-//  Created by 류희재 on 12/19/24.
-//  Copyright © 2024 Heylets-iOS. All rights reserved.
+//import Core
 //
-
-import Foundation
-import Combine
-
-import Core
-
-public protocol NavigationRoutable {
-    var destinations: [NavigationDestination] { get set }
-    
-    func push(to view: NavigationDestination)
-    func pop()
-    func popToRootView()
-}
+//public protocol NavigationRoutable {
+//    var destinations: [NavigationDestination] { get set }
+//    
+//    func push(to view: NavigationDestination)
+//    func pop()
+//    func popToRootView()
+//}
