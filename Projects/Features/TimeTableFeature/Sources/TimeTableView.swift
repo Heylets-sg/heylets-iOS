5be95cda4cdd719e4214c9f0e0ca738bf83299a2
HELLOHIDI <fbgmlwo123@naver.com>
2024-12-27 13:08:16 +0900

    [Setting] 코드 이동 및 파일 분배 #15

--- Projects/Features/TimeTableFeature/Sources/TimeTableView.swift
+++ Projects/Features/TimeTableFeature/Sources/TimeTableView.swift
@@ -2,17 +2,48 @@
 //  TimeTableView.swift
 //  TimeTableFeature
 //
-//  Created by 류희재 on 12/18/24.
+//  Created by 류희재 on 12/27/24.
 //  Copyright © 2024 Heylets-iOS. All rights reserved.
 //
 
 import SwiftUI
 
+import DSKit
+
 public struct TimeTableView: View {
     public init() {}
+    @State private var isShowingModuleDetailInfoView = false
+    @State private var isShowingSearchModuleView = false
+    
     public var body: some View {
-        VStack {
-            Text("TimeTableView")
+        ZStack {
+            VStack(alignment: .leading) {
+                if isShowingSearchModuleView {
+                    SearchModuleTopView(isShowingSearchModuleView: $isShowingSearchModuleView)
+                } else {
+                    TopView(isShowingSearchModuleView: $isShowingSearchModuleView)
+                }
+                
+                Spacer()
+                    .frame(height: 19)
+                
+                MainView(isShowingModuleDetailInfoView: $isShowingModuleDetailInfoView)
+            }
+            .onTapGesture {
+                isShowingModuleDetailInfoView = false
+                isShowingSearchModuleView = false
+            }
+        }
+        if isShowingModuleDetailInfoView {
+            DetailModuleInfoView(isShowingModuleDetailInfoView: $isShowingModuleDetailInfoView)
+                .zIndex(2)
+                .transition(.move(edge: .bottom))
+        }
+        
+        if isShowingSearchModuleView {
+            SearchModuleView()
+                .zIndex(2)
+                .transition(.move(edge: .bottom))
         }
     }
 }
