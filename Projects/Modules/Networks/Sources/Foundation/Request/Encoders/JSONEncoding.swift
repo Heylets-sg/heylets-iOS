9708e322388282100a1ba427c8062e6a72a551fd
HELLOHIDI <fbgmlwo123@naver.com>
2025-01-08 16:04:20 +0900

    [Feat] Builder 폴더 Encoding 파일 구현 #32

---
+++ Projects/Modules/Networks/Sources/Foundation/Request/Encoders/JSONEncoding.swift
@@ -0,0 +1,37 @@
+//
+//  JSONEncoding.swift
+//  Networks
+//
+//  Created by 류희재 on 1/8/25.
+//  Copyright © 2025 Heylets-iOS. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+public protocol JSONEncodingType {
+    func encode(_ request: URLRequest, with parameters: Encodable) -> AnyPublisher<URLRequest, HMHNetworkError.RequestError.ParameterEncodingError>
+}
+
+public struct JSONEncoding: JSONEncodingType {
+    public init() {}
+    
+    public func encode(_ request: URLRequest, with parameters: Encodable) -> AnyPublisher<URLRequest, HMHNetworkError.RequestError.ParameterEncodingError> {
+        
+        return Just(request)
+            .tryMap { request in
+                var modifiedRequest = request
+                
+                do {
+                    let data = try JSONEncoder().encode(parameters)
+                    modifiedRequest.httpBody = data
+                    return modifiedRequest
+                } catch {
+                    throw HMHNetworkError.RequestError.ParameterEncodingError.jsonEncodingFailed
+                }
+            }
+            .mapError { _ in HMHNetworkError.RequestError.ParameterEncodingError.unknownErr }
+            .eraseToAnyPublisher()
+    }
+}
+
