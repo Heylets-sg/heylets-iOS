659c198cada3114a9aec4a02089f51d172d11186
HELLOHIDI <fbgmlwo123@naver.com>
2025-01-08 16:02:20 +0900

    [Feat] Request 폴더 Config 파일 구현 #32

---
+++ Projects/Modules/Networks/Sources/Foundation/Request/Config/Paths.swift
@@ -0,0 +1,13 @@
+//
+//  Paths.swift
+//  Networks
+//
+//  Created by 류희재 on 1/8/25.
+//  Copyright © 2025 Heylets-iOS. All rights reserved.
+//
+
+import Foundation
+
+public enum Paths {
+    
+}
