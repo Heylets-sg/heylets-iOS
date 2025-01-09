21d6c47253711d0235daeae319479489c6de8b2f
HELLOHIDI <fbgmlwo123@naver.com>
2025-01-09 16:16:28 +0900

    [Setting] Repository 세팅 #36

---
+++ Projects/Domain/Sources/RepositoryInterface/TimeTableRepositoryType.swift
@@ -0,0 +1,9 @@
+//
+//  TimeTableRepositoryType.swift
+//  Domain
+//
+//  Created by 류희재 on 1/9/25.
+//  Copyright © 2025 Heylets-iOS. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Domain/Sources/RepositoryInterface/UserRepositoryType.swift
@@ -0,0 +1,9 @@
+//
+//  UserRepositoryType.swift
+//  Domain
+//
+//  Created by 류희재 on 1/9/25.
+//  Copyright © 2025 Heylets-iOS. All rights reserved.
+//
+
+import Foundation
