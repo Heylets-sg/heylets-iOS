6ff12439b2c49c3f07586ccb9300d4d23d0b8bdf
HELLOHIDI <fbgmlwo123@naver.com>
2025-01-10 18:08:21 +0900

    [Feat] SectionRepository 구현 #36

--- Projects/Data/Sources/Repository/SectionRepository.swift
+++ Projects/Data/Sources/Repository/SectionRepository.swift
@@ -6,16 +6,41 @@
 //  Copyright © 2025 Heylets-iOS. All rights reserved.
 //
 
-//import Foundation
-//import Combine
-//
-//import Domain
-//import Networks
-//
-//public struct SectionRepository: SectionRepositoryType {
-//    private let sectionService: SectionServiceType
-//    
-//    public init(sectionService: SectionServiceType) {
-//        self.sectionService = sectionService
-//    }
-//}
+import Foundation
+import Combine
+
+import Domain
+import Networks
+
+public struct SectionRepository: SectionRepositoryType {
+    private let service: SectionServiceType
+    
+    public init(service: SectionServiceType) {
+        self.service = service
+    }
+    
+    public func deleteAllSection(
+        _ tableId: String
+    ) -> AnyPublisher<Void, Error> {
+        service.deleteAllSection(tableId)
+            .asVoidWithGeneralError()
+    }
+    
+    public func deleteSection(
+        _ tableId: String,
+        _ sectionId: String
+    ) -> AnyPublisher<Void, Error> {
+        service.deleteSection(tableId, sectionId)
+            .asVoidWithGeneralError()
+    }
+    
+    public func addSection(
+        _ tableId: String,
+        _ sectionId: Int,
+        _ memo: String
+    ) -> AnyPublisher<Void, Error> {
+        let request: AddSectionRequest = .init(sectionId, memo)
+        return service.addSection(tableId, request)
+            .asVoidWithGeneralError()
+    }
+}
