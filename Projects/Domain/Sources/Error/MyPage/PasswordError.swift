e7aca728d3a23da44f9dd93154a370cf847bdef2
HELLOHIDI <fbgmlwo123@naver.com>
2025-01-11 11:29:03 +0900

    [Feat] MyPageUseCaseType 구현 #38

---
+++ Projects/Domain/Sources/Error/MyPage/PasswordError.swift
@@ -0,0 +1,14 @@
+//
+//  PasswordError.swift
+//  Domain
+//
+//  Created by 류희재 on 1/11/25.
+//  Copyright © 2025 Heylets-iOS. All rights reserved.
+//
+
+import Foundation
+
+public enum PasswordError {
+    case inValidCurrentPassword
+    case inValidCheckPassword
+}
