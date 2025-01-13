f0e44e45c906d8900f09ef4a218c60aa154ac628
HELLOHIDI <fbgmlwo123@naver.com>
2025-01-13 14:34:09 +0900

    [Delete] 필요없는 코드 삭제 #42

---
+++ Projects/App/Sources/DIContainer.swift
@@ -0,0 +1,14 @@
+//
+//  DIContainer.swift
+//  Heylets-iOS
+//
+//  Created by 류희재 on 1/13/25.
+//  Copyright © 2025 Heylets-iOS. All rights reserved.
+//
+
+import Foundation
+import BaseFeatureDependency
+
+extension DIContainer {
+    static public let `default` = DIContainer()
+}
---
+++ Projects/Features/BaseFeatureDependency/Sources/DIContainer.swift
@@ -0,0 +1,31 @@
+//
+//  DIContainer.swift
+//  Heylets-iOS
+//
+//  Created by 류희재 on 12/18/24.
+//  Copyright © 2024 Heylets-iOS. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+import Core
+
+public typealias NavigationRoutableType = NavigationRoutable & ObservableObjectSettable
+public typealias WindowRoutableType = WindowRoutable & ObservableObjectSettable
+
+final public class DIContainer: ObservableObject {
+    public var navigationRouter: NavigationRoutableType
+    public var windowRouter: WindowRoutableType
+    
+    public init(
+        navigationRouter: NavigationRoutableType = NavigationRouter(),
+        windowRouter: WindowRoutableType = WindowRouter()
+    ) {
+        self.navigationRouter = navigationRouter
+        self.windowRouter = windowRouter
+        
+        navigationRouter.setObjectWillChange(objectWillChange)
+        windowRouter.setObjectWillChange(objectWillChange)
+    }
+}
