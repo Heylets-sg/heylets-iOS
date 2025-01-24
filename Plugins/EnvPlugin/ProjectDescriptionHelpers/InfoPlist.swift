327536b29826c84c242e40a3bd25b37e4f6876a0
HELLOHIDI <fbgmlwo123@naver.com>
2025-01-24 20:01:54 +0900

    [Fix] 다크모드, 가로모드 방지

--- Plugins/EnvPlugin/ProjectDescriptionHelpers/InfoPlist.swift
+++ Plugins/EnvPlugin/ProjectDescriptionHelpers/InfoPlist.swift
@@ -11,25 +11,33 @@ import ProjectDescription
 /// InfoPList를 정리해둔 파일이빈다
 public extension Project {
     static let appInfoPlist: [String: Plist.Value] = [
-        "BASE_URL": "$(BASE_URL)",
-        "NSAppTransportSecurity": [
-            "NSAllowsArbitraryLoads": true
-        ],
-        "UIAppFonts": [
-            "NotoSansKR-Medium.ttf",
-            "NotoSansKR-Regular.ttf",
-            "NotoSansKR-SemiBold.ttf",
-            "NotoSansKR-Bold.ttf"
-        ],
-        "UILaunchStoryboardName": "LaunchScreen",
-        // Photo library usage description
-        "NSPhotoLibraryUsageDescription": "This app requires access to the photo library to select and upload photos.",
-        // Notification usage description
-        "UIUserNotificationUsageDescription": "The app requests permission to send notifications.",
-        // Camera usage description
-        "NSCameraUsageDescription": "This app requires access to the camera to take and upload photos."
-    ]
-    
+            "BASE_URL": "$(BASE_URL)",
+            "NSAppTransportSecurity": [
+                "NSAllowsArbitraryLoads": true
+            ],
+            "UIAppFonts": [
+                "NotoSansKR-Medium.ttf",
+                "NotoSansKR-Regular.ttf",
+                "NotoSansKR-SemiBold.ttf",
+                "NotoSansKR-Bold.ttf"
+            ],
+            "UILaunchStoryboardName": "LaunchScreen",
+            // Photo library usage description
+            "NSPhotoLibraryUsageDescription": "This app requires access to the photo library to select and upload photos.",
+            // Notification usage description
+            "UIUserNotificationUsageDescription": "The app requests permission to send notifications.",
+            // Camera usage description
+            "NSCameraUsageDescription": "This app requires access to the camera to take and upload photos.",
+            // Disable dark mode
+            "UIUserInterfaceStyle": "Light",
+            // Restrict to portrait orientation
+            "UISupportedInterfaceOrientations": [
+                "UIInterfaceOrientationPortrait"
+            ],
+            "UISupportedInterfaceOrientations~ipad": [
+                "UIInterfaceOrientationPortrait"
+            ]
+        ]
     
     static let demoInfoPlist: [String: Plist.Value] = [
         "BASE_URL": "$(BASE_URL)",
