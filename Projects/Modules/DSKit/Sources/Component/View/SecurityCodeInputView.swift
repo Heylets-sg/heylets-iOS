40fc2a7a37506a8ed73d85d97a1430223600971f
HELLOHIDI <fbgmlwo123@naver.com>
2025-02-12 21:09:58 +0900

    [Fix] 인증번호 입력창 레이아웃 오류 해결 #60

--- Projects/Modules/DSKit/Sources/Component/View/SecurityCodeInputView.swift
+++ Projects/Modules/DSKit/Sources/Component/View/SecurityCodeInputView.swift
@@ -19,56 +19,68 @@ public struct SecurityCodeInputView: View {
     
     //MARK: Body
     public var body: some View {
-        HStack {
-            ZStack(alignment: .center) {
-                TextField("", text: $otpCode)
-                    .frame(width: 0, height: 0, alignment: .center)
-                    .font(Font.system(size: 0))
-                    .accentColor(.clear)
-                    .foregroundColor(.clear)
-                    .multilineTextAlignment(.center)
-                    .keyboardType(.numberPad)
-                    .onChange(of: otpCode) { newValue in
-                        print(otpCode)
-                        if otpCode.isEmpty {
-                            self.focusedField = .field // otpCode가 비어 있으면 키보드 올리기
+//        GeometryReader { geometry in
+//            let cellWidth: CGFloat = geometry.size.width / 10
+//            let padding: CGFloat = geometry.size.width / 30
+            
+            HStack {
+                ZStack(alignment: .center) {
+                    TextField("", text: $otpCode)
+                        .frame(width: 0, height: 0, alignment: .center)
+                        .font(Font.system(size: 0))
+                        .accentColor(.clear)
+                        .foregroundColor(.clear)
+                        .multilineTextAlignment(.center)
+                        .keyboardType(.numberPad)
+                        .onChange(of: otpCode) { newValue in
+                            print(otpCode)
+                            if otpCode.isEmpty {
+                                self.focusedField = .field // otpCode가 비어 있으면 키보드 올리기
+                            }
+                            if otpCode.count >= 6 {
+                                otpCode = String(otpCode.prefix(6))
+                                
+                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                            }
                         }
-                        if otpCode.count >= 6 {
-                            otpCode = String(otpCode.prefix(6))
-                            
-                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        .focused($focusedField, equals: .field)
+                        .task {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5)
+                            {
+                                self.focusedField = .field
+                            }
                         }
+                        .padding()
+                        
+                    HStack(spacing: 0) {
+                        
+                        ForEach(0..<6) { index in
+                            VStack {
+                                Text(self.getPin(at: index))
+                                    .font(.semibold_28)
+                                    .foregroundColor(Color.heyGray1)
+                                Rectangle()
+                                    .frame(width: 32, height: 2)
+                                    .foregroundColor(Color.heyGray1)
+                                    .padding(.leading, index == 3 ? 20 : 10)
+                                    .padding(.trailing, index == 2 ? 20 : 10)
+                                    .opacity(self.otpCode.count <= index ? 1 : 0)
+                                    
+                            }
+                        }
+                        
+                        
                     }
-                    .focused($focusedField, equals: .field)
-                    .task {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5)
-                        {
-                            self.focusedField = .field
-                        }
-                    }
-                    .padding()
-                HStack {
-                    ForEach(0..<6) { index in
-                        VStack {
-                            Text(self.getPin(at: index))
-                                .font(.semibold_28)
-                                .foregroundColor(Color.heyGray1)
-                            Rectangle()
-                                .frame(width: 32, height: 2)
-                                .foregroundColor(Color.heyGray1)
-                                .padding(.leading, index == 3 ? 20 : 10)
-                                .padding(.trailing, index == 2 ? 20 : 10)
-                                .opacity(self.otpCode.count <= index ? 1 : 0)
-                        }
-                    }
+                    
                 }
             }
+            .onTapGesture {
+                self.focusedField = .field
+            }
         }
-        .onTapGesture {
-            self.focusedField = .field
-        }
+        
     }
-}
+//}
 
 extension SecurityCodeInputView {
     //MARK: func
@@ -107,7 +119,7 @@ struct ContentView: View {
             
             SecurityCodeInputView(otpCode: $otpCode)
                 .frame(height: 50)
-                .padding()
+                .padding(.horizontal, 40)
         }
         .padding()
     }
