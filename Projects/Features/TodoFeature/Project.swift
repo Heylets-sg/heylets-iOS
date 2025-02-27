87731e7d884efe85cbdf4f3be9c13472f3f300b4
HELLOHIDI <fbgmlwo123@naver.com>
2025-02-27 13:21:56 +0900

    [Feat] Todo 모듈 생성 #72

---
+++ Projects/Features/TodoFeature/Project.swift
@@ -0,0 +1,18 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 류희재 on 2/27/25.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: "TodoFeature",
+    targets: [.dynamicFramework, .interface], //, .demo
+    interfaceDependencies: [
+        .Features.BaseFeatureDependency
+    ]
+)
