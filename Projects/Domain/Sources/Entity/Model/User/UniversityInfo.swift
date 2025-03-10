98e7f06bc14f3552107e85f86fef6a5750d46808
HELLOHIDI <fbgmlwo123@naver.com>
2025-03-10 14:43:39 +0900

    [Feat] UniversityInfo 텍스트, 배경 색상 추가 #74

---
+++ Projects/Domain/Sources/Entity/Model/User/UniversityInfo.swift
@@ -0,0 +1,15 @@
+//
+//  UniversityInfo.swift
+//  Domain
+//
+//  Created by 류희재 on 3/10/25.
+//  Copyright © 2025 Heylets-iOS. All rights reserved.
+//
+
+import Foundation
+
+public enum UniversityInfo: String {
+    case NUS
+    case NTU
+    case SMU
+}
---
+++ Projects/Features/BaseFeatureDependency/Sources/Model/UniversityInfo.swift
@@ -0,0 +1,48 @@
+//
+//  UniversityInfo.swift
+//  BaseFeatureDependency
+//
+//  Created by 류희재 on 3/10/25.
+//  Copyright © 2025 Heylets-iOS. All rights reserved.
+//
+
+import UIKit
+
+import Domain
+import DSKit
+
+
+extension UniversityInfo {
+    public var icon: UIImage {
+        switch self {
+        case .NUS:
+            return .nus
+        case .NTU:
+            return .ntu
+        case .SMU:
+            return .smu
+        }
+    }
+    
+    public var textColor: String {
+        switch self {
+        case .NUS:
+            return "#DB812E"
+        case .NTU:
+            return "#FFFFFF"
+        case .SMU:
+            return "#FFFFFF"
+        }
+    }
+    
+    public var backgroundColor: String {
+        switch self {
+        case .NUS:
+            return "#1B3D76"
+        case .NTU:
+            return "#E01932"
+        case .SMU:
+            return "#1B286A"
+        }
+    }
+}
