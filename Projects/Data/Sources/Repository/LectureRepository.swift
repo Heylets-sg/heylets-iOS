87ab93266b6b94bd87fa35c3c091a338be8b8e1d
HELLOHIDI <fbgmlwo123@naver.com>
2025-08-05 11:44:26 +0900

    [Feat] 강의목록 캐싱 구현 #113

--- Projects/Data/Sources/Repository/LectureRepository.swift
+++ Projects/Data/Sources/Repository/LectureRepository.swift
@@ -15,9 +15,14 @@ import Networks
 public struct LectureRepository: LectureRepositoryType {
 
     private let service: LectureServiceType
+    private let cacheManager: LectureCacheManager
     
-    public init(service: LectureServiceType) {
+    public init(
+        service: LectureServiceType,
+        cacheManager: LectureCacheManager
+    ) {
         self.service = service
+        self.cacheManager = cacheManager
     }
     
     public func getLectureDetailInfo(
@@ -31,10 +36,30 @@ public struct LectureRepository: LectureRepositoryType {
     public func getLectureList(
         _ filterInfo: FilterInfo
     ) -> AnyPublisher<[SectionInfo], Error> {
-        let params = filterInfo.toRequestParameters()
-        return service.getLectureList(params)
-            .map { $0.content.flatMap { $0.toEntity().sections } }
-            .mapToGeneralError()
+        let cacheKey = cacheManager.makeLecturesCacheKey(department: filterInfo.department)
+        
+        return cacheManager.getCachedLectures(for: cacheKey, page: filterInfo.page)
+            .flatMap { [service, cacheManager] cachedInfo -> AnyPublisher<[SectionInfo], Error> in
+                if let cachedInfo = cachedInfo {
+                    print("✅ Cache HIT for cacheKey: \(cacheKey) page: 0 ~ \(filterInfo.page)")
+                    return Just(cachedInfo)
+                        .setFailureType(to: Error.self)
+                        .eraseToAnyPublisher()
+                } else {
+                    print("❌ Cache MISS for cacheKey: \(cacheKey) - Fetching from server")
+                    let params = filterInfo.toRequestParameters()
+                    return service.getLectureList(params)
+                        .map { $0.content.flatMap { $0.toEntity().sections } }
+                        .handleEvents(receiveOutput: {
+                            cacheManager.appendLectures($0, for: cacheKey, page: filterInfo.page)
+                        })
+                        .mapToGeneralError()
+                }
+            }
+            .eraseToAnyPublisher()
+        
+        
+        
     }
     
     public func getLectureDepartment(
